d72e50b13e86bbadc0fe069df7fa66ca54357ce3
dhimanparas20 <dhimanparas20@gmail.com>
2022-02-06 17:16:40 +0530

    sweet: Update havoc.dependencies
    Signed-off-by: dhimanparas20 <dhimanparas20@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,8 +16,8 @@ TARGET_OTA_ASSERT_DEVICE := sweet,sweetin
 TARGET_PROVIDES_AUDIO_EXTNS := true
 
 # Init
-TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_sweet
-TARGET_RECOVERY_DEVICE_MODULES := libinit_sweet
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_sweet
+TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_sweet
 
 # Kernel
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sm6150
