8785c7d859a978d1c2fd72bf7421a2bb107f67c7
dhimanparas20 <dhimanparas20@gmail.com>
2022-02-21 13:59:19 +0530

    sweet: Havoc-OS bringup
    Signed-off-by: dhimanparas20 <dhimanparas20@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_sweet.mk
+    $(LOCAL_DIR)/havoc_sweet.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_sweet-eng \
-    lineage_sweet-userdebug \
-    lineage_sweet-user
+    havoc_sweet-eng \
+    havoc_sweet-userdebug \
+    havoc_sweet-user
